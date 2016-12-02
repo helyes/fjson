67c80216cc16447a96a860cf2562f54274a615cc
helyes <helyesa@gmail.com>
2016-12-03 09:41:25 +1100

    Initial commit, project skeleton

---
+++ FJson/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FJson
+//
+//  Created by Andras Helyes on 3/12/16.
+//  Copyright © 2016 11toes. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
